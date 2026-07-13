d6483ac38bf4befd49627c7293fbd7f91cfb0a15
DESU DEEPESH <desudeepeshacad@gmail.com>
2026-07-13 16:20:57 +0530

    Add files via upload

---
+++ WEEK-3/Module_5/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package week3.Module_5.LibraryManagement.src.main.java.com.library.repository;
+
+public class BookRepository {
+    public void save() {
+        System.out.println("BookRepository: Saving book to the database...");
+    }
+}
