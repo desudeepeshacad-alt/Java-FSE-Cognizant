665aa2867afcb9ef5b342a0f9d7caeb120e915e7
DESU DEEPESH <desudeepeshacad@gmail.com>
2026-07-13 11:41:22 +0530

    Add files via upload

---
+++ WEEK-1/Exercise_1_Mocking_and_Stubbing/MyService.java
@@ -0,0 +1,12 @@
+package com.cognizant.mockito.exercise1;
+
+public class MyService {
+
+    private ExternalApi externalApi;
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}
